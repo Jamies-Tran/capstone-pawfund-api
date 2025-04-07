9f1c9c8eb888b4544b39cc59c075eec017071f45
Jamies-Tran <minhtranquang2197@gmail.com>
2025-04-07 15:43:38 +0700

    Commit code 07-04-2025 | v1.1.3

--- src/main/java/com/paw/fund/app/modules/account_management/controller/models/AccountRoleRegisterRequest.java
+++ src/main/java/com/paw/fund/app/modules/account_management/controller/models/AccountRoleRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.paw.fund.app.modules.account_management.controller.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -7,12 +8,17 @@ import java.time.LocalDate;
 
 @Builder
 public record AccountRoleRegisterRequest(
+        @Schema(description = "Ngày sinh của người dùng", example = "1997-01-02")
         LocalDate dateOfBirth,
+        @Schema(description = "Mã giới tính", example = "MALE")
         String genderCode,
+        @Schema(description = "Tên giới tính", example = "Nam")
         String genderName,
         @NotNull(message = "Vui lòng nhập địa chỉ")
+        @Schema(description = "Địa chỉ của người dùng", example = "43 Phan Văn Trị, phường 2, quận 5, TP Hồ Chí Minh")
         String address,
         @NotNull(message = "Vui lòng nhập CCCD")
+        @Schema(description = "Số CCCD của người dùng", example = "079097015437")
         String identification
 ) {
 }
--- src/main/java/com/paw/fund/app/modules/account_management/controller/v2/IAccountV2API.java
+++ src/main/java/com/paw/fund/app/modules/account_management/controller/v2/IAccountV2API.java
@@ -4,13 +4,24 @@ import com.paw.fund.app.modules.account_management.controller.models.AccountResp
 import com.paw.fund.app.modules.account_management.controller.models.AccountRoleRegisterRequest;
 import com.paw.fund.app.modules.account_management.controller.models.AccountUpdateRequest;
 import com.paw.fund.utils.response.ValueResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/v2/api/account")
 @Tag(name = "Account V2", description = "QL tài khoản")
 public interface IAccountV2API {
-    @PatchMapping("/register-adoptor")
-    ValueResponse<AccountResponse> registerAdopterRole(AccountRoleRegisterRequest request);
+    @PutMapping("/register-adoptor")
+    @Operation(
+            summary = "Đăng ký quyền nhận nuôi thú cưng",
+            description = """
+                    - Người dùng đã xác thực đăng ký quyền nhận nuôi thú cưng
+                    - [AUTHENTICATED - Người dùng đã xác thực]
+                    """)
+    ValueResponse<AccountResponse> registerAdopterRole(@RequestBody @Valid AccountRoleRegisterRequest request);
 }
